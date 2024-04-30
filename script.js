d5681ef2653081e7aec9d05a0c8ecfffa2b8b6af
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-04-30 18:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	str = str.toLowerCase();
+	if (str === ""){
+		return null;
+	}
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i]
+	if (str.indexOf(char)=== str.lastIndexOf(char)) {
+		return char
+	}
+		}
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
